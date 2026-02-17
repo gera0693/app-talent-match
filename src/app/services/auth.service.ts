974133d6e373dc2105df85fd03f2752c86cd8915
Gerardo Villalobos <gera0693@gmail.com>
2026-02-17 08:50:50 -0600

    Add login feature

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,56 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { AuthUser } from '../models/auth-user.model';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private STORAGE_KEY = 'demo_user';
+  private router = inject(Router);
+
+  currentUser = signal<AuthUser | null>(this.getStoredUser());
+
+  private users = [
+    { username: 'admin', password: '1234', role: 'admin' as const },
+    { username: 'user', password: '1234', role: 'user' as const }
+  ];
+
+  login(username: string, password: string): AuthUser | null {
+    const found = this.users.find(
+      u => u.username === username && u.password === password
+    );
+
+    if (!found) return null;
+
+    const authUser: AuthUser = {
+      username: found.username,
+      role: found.role
+    };
+
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(authUser));
+    this.currentUser.set(authUser);
+
+    return authUser;
+  }
+
+  logout() {
+    localStorage.removeItem(this.STORAGE_KEY);
+    this.currentUser.set(null);
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.currentUser();
+  }
+
+  hasRole(role: 'admin' | 'user'): boolean {
+    return this.currentUser()?.role === role;
+  }
+
+  private getStoredUser(): AuthUser | null {
+    const data = localStorage.getItem(this.STORAGE_KEY);
+    return data ? JSON.parse(data) : null;
+  }
+}
